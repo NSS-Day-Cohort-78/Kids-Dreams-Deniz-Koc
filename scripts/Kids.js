4740db0cc41de510519e1af6551ddacf8b339cb4
Deniz Koc <dkocusa@outlook.com>
2025-08-08 18:49:13 -0500

    complete Kids Dreams self-assessment

--- scripts/Kids.js
+++ scripts/Kids.js
@@ -6,10 +6,18 @@ export const Kids = () => {
     let html = "<ol>"
 
     for (const child of children) {
-        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child.name}</li>`
-    }
+        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}" data-name="${child.name}">${child.name}</li>`}
 
     html += "</ol>"
     return html
 }
 
+document.addEventListener(
+    "click",
+    (clickEvent) =>{
+        const itemClicked = clickEvent.target
+    if (itemClicked.dataset.type === "child") {
+        window.alert(`${itemClicked.dataset.name}' wish is ${itemClicked.dataset.wish}`)
+        }
+    }
+)
